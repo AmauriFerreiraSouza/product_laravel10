f54547cbc6ce6ab8a08d3dbe47a860f56f3e400d
Amauri Ferreira de Souza <amauridesouzaferreira@gmail.com>
2023-05-01 00:09:52 -0300

    Using CREATE method in eleoquent to write product data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,19 +21,28 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        $product = new Product;
+        // $product = new Product;
 
         $image_name = time().'.'. request()->image->getClientOriginalExtension();
 
         request()->image->move(public_path('images'), $image_name);
 
-        $product->name = $request->name;
-        $product->description = $request->description;
-        $product->image = $image_name;
-        $product->category = $request->category;
-        $product->quantity = $request->quantity;
-        $product->price = $request->price;
-        $product->save();
+        Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $image_name,
+            'category' => $request->category,
+            'quantity' => $request->quantity,
+            'price' => $request->price
+        ]);
+
+        // $product->name = $request->name;
+        // $product->description = $request->description;
+        // $product->image = $image_name;
+        // $product->category = $request->category;
+        // $product->quantity = $request->quantity;
+        // $product->price = $request->price;
+        // $product->save();
         return redirect()->route('products.index')->with('success', 'Produto adicionado com Sucesso!');
     }
 }
